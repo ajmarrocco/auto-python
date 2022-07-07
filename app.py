80ad437f30f439a4817e429b0e1cbb6b1cd232ec
ajmarrocco <ajmarrocco@gmail.com>
2022-07-06 21:54:33 -0400

    refractor

--- app.py
+++ app.py
@@ -1,19 +1,33 @@
 import openpyxl as xl
+from openpyxl.chart import BarChart, Reference
 
-# Loads excel workbook
-wb = xl.load_workbook('transactions.xlsx')
-# Gets sheet 
-sheet = wb['Sheet1']
-# Gets cell
-cell = sheet['a1']
+def process_workbook(filename):
+    # Loads excel workbook
+    wb = xl.load_workbook('transactions.xlsx')
+    # Gets sheet 
+    sheet = wb['Sheet1']
+    # Gets cell
+    cell = sheet['a1']
 
-for row in range(2, sheet.max_row + 1):
-    # gets what is in each cell in the row
-    # .cell(row, column)
-    cell = sheet.cell(row, 3)
-    corrected_price = cell.value * 0.9
-    corrected_price_cell = sheet.cell(row,4)
-    corrected_price_cell.value = corrected_price
+    for row in range(2, sheet.max_row + 1):
+        # gets what is in each cell in the row
+        # .cell(row, column)
+        cell = sheet.cell(row, 3)
+        corrected_price = cell.value * 0.9
+        corrected_price_cell = sheet.cell(row,4)
+        corrected_price_cell.value = corrected_price
 
-# Saves in new file 
-wb.save('transaction2.xlsx')
+
+    values = Reference(sheet, 
+                        min_row =2, 
+                        max_row = sheet.max_row,
+                        min_col=4,
+                        max_col = 4
+                        )
+
+    chart = BarChart()
+    chart.add_data(values)
+    sheet.add_chart(chart, 'e2')
+
+    # Saves in new file 
+    wb.save('transactions.xlsx')
